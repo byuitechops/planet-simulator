bd8188745e7746fccf524fcc9e791e810a521d76
Jay Hanson <jshanson88@gmail.com>
2016-11-10 16:03:20 -0700

    fixes #34 fixes #32 fixes #31 fixes #10 fixes #7

--- main.js
+++ main.js
@@ -1,201 +1,7 @@
 /*jslint plusplus: true, browser: true, devel: true */
 /*global $:false, Frame:false, Animator:false, moveLightBeam:false, transitionBoxen:false, createSpotlight:false, getCSV:false*/
 var boxen, animations,
-    animationInProgress = false,
-    containers = [
-        {
-            name: "sea",
-            isFrame: true,
-            items: 5,
-            width: 392,
-            height: 392,
-            x: 205,
-            y: 351,
-            path: "./images/animations/flooding/flood",
-            ext: ".png",
-            scale: 1.1,
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "ice",
-            isFrame: false,
-            items: 5,
-            width: 304,
-            height: 392,
-            x: 253,
-            y: 350,
-            path: "./images/animations/iceCaps/iceCap",
-            ext: ".png",
-            scale: 1.1,
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "insolation",
-            isFrame: true,
-            items: 5,
-            width: 334,
-            height: 412,
-            x: -4,
-            y: 214,
-            path: "./images/animations/lightRays/lightRay",
-            ext: ".png",
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "mountain",
-            isFrame: false,
-            items: 5,
-            width: 187,
-            height: 238,
-            x: 155,
-            y: 456,
-            path: "./images/animations/mountains/mountain",
-            ext: ".png",
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "volcano",
-            isFrame: true,
-            items: 5,
-            width: 325,
-            height: 322,
-            x: 274,
-            y: 425,
-            path: "./images/animations/volcanoes/volcano",
-            ext: ".png",
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "co2",
-            isFrame: true,
-            items: 9,
-            width: 70,
-            height: 135,
-            x: 482,
-            y: 281,
-            path: "./images/animations/co2Meter/co2Meter",
-            ext: ".png",
-            scale: 4.3,
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "temperature",
-            isFrame: true,
-            items: 9,
-            width: 70,
-            height: 135,
-            x: 482,
-            y: 281,
-            path: "./images/animations/tempMeter/tempMeter",
-            ext: ".png",
-            scale: 4.25,
-            macaroni: {
-                needed: false
-            }
-    },
-        {
-            name: "underwaterVolcano",
-            isFrame: true,
-            items: 5,
-            width: 164,
-            height: 305,
-            x: 1287,
-            y: 419,
-            path: "./images/animations/underwaterVolcanoes/volcano",
-            ext: ".gif",
-            macaroni: {
-                needed: true,
-                x: 1386,
-                y: 653,
-                malicious: false,
-                name: "underwaterVolcano",
-            }
-    },
-        {
-            name: "co3Desposition",
-            isFrame: true,
-            items: 5,
-            width: 120,
-            height: 216,
-            x: 1165,
-            y: 564,
-            path: "./images/animations/seaSnow/seaSnow",
-            ext: ".gif",
-            macaroni: {
-                needed: true,
-                x: 1215,
-                y: 624,
-                malicious: false,
-                name: "co3Desposition"
-            }
-    },
-        {
-            name: "sediment",
-            isFrame: true,
-            items: 5,
-            width: 129,
-            height: 129,
-            x: 978,
-            y: 527,
-            path: "./images/animations/sediment/sediment",
-            ext: ".gif",
-            macaroni: {
-                needed: true,
-                x: 979,
-                y: 642,
-                malicious: false,
-                name: "sediment",
-            }
-    },
-        {
-            name: "weatheringCRelease",
-            isFrame: true,
-            items: 5,
-            width: 204,
-            height: 168,
-            x: 1396,
-            y: 396,
-            path: "./images/animations/carbonateRock/carbonateRock",
-            ext: ".gif",
-            macaroni: {
-                needed: true,
-                x: 1408,
-                y: 463,
-                malicious: false,
-                name: "weatheringCRelease",
-
-            }
-    }, {
-            name: "weatheringCBurial",
-            isFrame: true,
-            items: 5,
-            width: 50,
-            height: 50,
-            x: 994,
-            y: 485,
-            path: "./images/animations/empty/empty",
-            ext: ".png",
-            macaroni: {
-                needed: true,
-                x: 994,
-                y: 485,
-                malicious: false,
-                name: "weatheringCBurrial",
-            }
-    }
-];
+    animationInProgress = false;
 
 function setForcers(forcerObj) {
     "use strict";
@@ -262,7 +68,7 @@ function checkAnimationStatus() {
 function updateBoxen(stepData) {
     'use strict';
 
-    var properOrder = [2, 3, 1, 0, 4, 5, 6, 11, 9, 8, 7, 10];
+    var properOrder = [3, 4, 2, 0, 1, 5, 6, 11, 9, 8, 7, 10];
 
     // Determine which animations need to fire
     animations = properOrder.filter(function (val) {
@@ -291,7 +97,9 @@ function updateBoxen(stepData) {
     if (animations.length > 0) {
         moveSpotlight();
     } else {
-        window.setTimeout(animationsComplete, 2000);
+        $('#noChangeMessage').delay(500).fadeIn(800, function() {
+            $('#noChangeMessage').delay(1000).fadeOut(800, animationsComplete);
+        });
     }
 
 }
@@ -324,7 +132,7 @@ function init(forcerObj, timeScaleOps) {
 
     // modified to make text clickable too
     $("a, a + * + text").on("click", function () {
-        if (animationInProgress || $(this).hasClass("active")) {
+        if (animationInProgress || $(this.parentElement).hasClass("active")) {
             return;
         }
 
